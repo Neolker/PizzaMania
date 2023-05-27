2bbc260b6591138380c1d22f441c056a16442b18
Marek Hovi Hovančák <hovimarcus@gmail.com>
2023-05-27 12:09:21 +0200

    Create ABL for ingredient

---
+++ server/abl/ingredient/list-abl.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const Ajv = require("ajv").default;
+const IngredientDao = require("../../dao/ingredient-dao");
+let dao = new IngredientDao(
+  path.join(__dirname, "..", "..", "storage", "ingredients.json")
+);
+
+async function ListAbl(req, res) {
+  try {
+    const ingredients = await dao.listIngredients();
+    res.json(ingredients);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+}
+
+module.exports = ListAbl;
