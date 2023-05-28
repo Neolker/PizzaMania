c1d98059139844785df1b5265e628eef429cf057
milos-hynek <134530320+milos-hynek@users.noreply.github.com>
2023-05-28 17:00:37 +0200

    Chybové hlášky, validace vstupů, ingredience
    Ošetření vstupů, odebrání ingrediencí - budou se řešit přes separátní fce, sjednocení chybových hlášek.

--- server/dao/recipe-dao.js
+++ server/dao/recipe-dao.js
@@ -18,13 +18,22 @@ class RecipeDao {
 
   async createRecipe(recipe) {
     let recipeList = await this._loadAllRecipes();
-    recipe.id = "REC-" + crypto.randomBytes(4).toString("hex");
-    recipeList.push(recipe);
-    await wf(
-      this._getStorageLocation(),
-      JSON.stringify(recipeList, null, 2)
-    );
-    return recipe;
+    let recipePrototype={
+      "id":"REC-" + crypto.randomBytes(4).toString("hex"),
+      "name":"New recipe",
+      "description":"The best recipe ever.",
+      "procedure":"Make it great.",
+      "ingredients":[]
+      };
+    if(recipe.name.length<2||recipe.description.length<2||recipe.procedure.length<2){
+      throw new Error("Name, description and procedure are required, recipe (name: "+recipe.name+", description: "+recipe.description+", procedure: "+recipe.procedure+") has not been created. Minimal lenght: 2 characters in every variable.");
+      }  
+    recipePrototype.name=recipe.name;
+    recipePrototype.description=recipe.description;
+    recipePrototype.procedure=recipe.procedure;
+    recipeList.push(recipePrototype);
+    await wf(this._getStorageLocation(),JSON.stringify(recipeList, null, 2));
+    return recipePrototype;
   }
 
   async getRecipe(id) {
@@ -35,23 +44,23 @@ class RecipeDao {
 
   async updateRecipe(recipe) {
     let recipeList = await this._loadAllRecipes();
-    const recipeIndex = recipeList.findIndex(
-      (b) => b.id === recipe.id
-    );
+    const recipeIndex = recipeList.findIndex( (b) => b.id === recipe.id );
     if (recipeIndex < 0) {
-      throw new Error(
-        `recipe with given id ${recipe.id} does not exists`
-      );
+      throw new Error("Recipe with given id "+recipe.id+" does not exists.");
     } else {
+      if(recipe.name.length<2||recipe.description.length<2||recipe.procedure.length<2){
+        throw new Error("Name, description and procedure are required, recipe (name: "+recipe.name+", description: "+recipe.description+", procedure: "+recipe.procedure+") has not been updated. Minimal lenght: 2 characters in every variable.");
+        } 
+      let recipePrototype=recipeList[recipeIndex];
+      recipePrototype.name=recipe.name;
+      recipePrototype.description=recipe.description;
+      recipePrototype.procedure=recipe.procedure;
       recipeList[recipeIndex] = {
         ...recipeList[recipeIndex],
-        ...recipe,
+        ...recipePrototype,
       };
     }
-    await wf(
-      this._getStorageLocation(),
-      JSON.stringify(recipeList, null, 2)
-    );
+    await wf(this._getStorageLocation(),JSON.stringify(recipeList, null, 2));
     return recipeList[recipeIndex];
   }
 
@@ -60,11 +69,10 @@ class RecipeDao {
     const recipeIndex = recipeList.findIndex((b) => b.id === id);
     if (recipeIndex >= 0) {
       recipeList.splice(recipeIndex, 1);
+    } else {
+      throw new Error("Recipe id "+id+" is not found.");
     }
-    await wf(
-      this._getStorageLocation(),
-      JSON.stringify(recipeList, null, 2)
-    );
+    await wf(this._getStorageLocation(),JSON.stringify(recipeList, null, 2));
     return {};
   }
 
@@ -92,14 +100,13 @@ class RecipeDao {
     try {
       recipeList = JSON.parse(await rf(this._getStorageLocation()));
     } catch (e) {
-      if (e.code === "ENOENT") {
+      if (typeof e.code === 'undefined') {
+        throw new Error("Unable to read from storage - wrong data format in "+this._getStorageLocation());
+      } else if (e.code === "ENOENT") {
         console.info("No storage found, initializing new one...");
         recipeList = [];
       } else {
-        throw new Error(
-          "Unable to read from storage. Wrong data format. " +
-          this._getStorageLocation()
-        );
+        throw new Error("Unable to read from storage - Wrong data format in " + this._getStorageLocation());
       }
     }
     return recipeList;
