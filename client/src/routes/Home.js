80242f8e68ed90636c621ab2ed8e39e4f895e4cf
Neolker <neolker@gmail.com>
2023-05-28 15:04:17 +0200

    Uprava karet/gridu a pridani receptu

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,21 +1,23 @@
-import {useContext, useEffect, useState} from "react";
+import { useContext, useEffect, useState } from "react";
 import UserContext from "../UserProvider";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Icon from "@mdi/react";
-import {mdiAlertOctagonOutline} from "@mdi/js";
+import { mdiAlertOctagonOutline } from "@mdi/js";
 import Container from "react-bootstrap/Container";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Placeholder from 'react-bootstrap/Placeholder';
 import Stack from 'react-bootstrap/Stack';
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Home() {
 
     const [listRecipeCall, setListRecipeCall] = useState({
         state: "pending",
     });
-    const {user, users, changeUser, getClassroomsToShow, isLoggedIn} = useContext(UserContext);
+    const { user, users, changeUser, getClassroomsToShow, isLoggedIn } = useContext(UserContext);
 
     let navigate = useNavigate();
 
@@ -25,9 +27,9 @@ function Home() {
         }).then(async (response) => {
             const responseJson = await response.json();
             if (response.status >= 400) {
-                setListRecipeCall({state: "error", error: responseJson});
+                setListRecipeCall({ state: "error", error: responseJson });
             } else {
-                setListRecipeCall({state: "success", data: responseJson});
+                setListRecipeCall({ state: "success", data: responseJson });
             }
         });
     }, []);
@@ -36,53 +38,54 @@ function Home() {
         switch (listRecipeCall.state) {
             case "pending":
                 return (<Stack direction="horizontal" gap={3}>
-                    <Card style={{width: '18rem'}}>
-                        <Card.Img variant="top" src="holder.js/100px180"/>
+                    <Card style={{ width: '18rem' }}>
+                        //TODO IMAGE
                         <Card.Body>
                             <Placeholder as={Card.Title} animation="glow">
-                                <Placeholder xs={6}/>
+                                <Placeholder xs={6} />
                             </Placeholder>
                             <Placeholder as={Card.Text} animation="glow">
-                                <Placeholder xs={7}/> <Placeholder xs={4}/> <Placeholder xs={4}/>{' '}
-                                <Placeholder xs={6}/> <Placeholder xs={8}/>
+                                <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+                                <Placeholder xs={6} /> <Placeholder xs={8} />
                             </Placeholder>
-                            <Placeholder.Button variant="primary" xs={6}/>
+                            <Placeholder.Button variant="primary" xs={6} />
                         </Card.Body>
                     </Card>
-                    <Card style={{width: '18rem'}}>
-                        <Card.Img variant="top" src="holder.js/100px180"/>
+                    <Card style={{ width: '18rem' }}>
+                        //TODO IMAGE
                         <Card.Body>
                             <Placeholder as={Card.Title} animation="glow">
-                                <Placeholder xs={6}/>
+                                <Placeholder xs={6} />
                             </Placeholder>
                             <Placeholder as={Card.Text} animation="glow">
-                                <Placeholder xs={7}/> <Placeholder xs={4}/> <Placeholder xs={4}/>{' '}
-                                <Placeholder xs={6}/> <Placeholder xs={8}/>
+                                <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+                                <Placeholder xs={6} /> <Placeholder xs={8} />
                             </Placeholder>
-                            <Placeholder.Button variant="primary" xs={6}/>
+                            <Placeholder.Button variant="primary" xs={6} />
                         </Card.Body>
                     </Card>
-                    <Card style={{width: '18rem'}}>
-                        <Card.Img variant="top" src="holder.js/100px180"/>
+                    <Card style={{ width: '18rem' }}>
+                        //TODO IMAGE
                         <Card.Body>
                             <Placeholder as={Card.Title} animation="glow">
-                                <Placeholder xs={6}/>
+                                <Placeholder xs={6} />
                             </Placeholder>
                             <Placeholder as={Card.Text} animation="glow">
-                                <Placeholder xs={7}/> <Placeholder xs={4}/> <Placeholder xs={4}/>{' '}
-                                <Placeholder xs={6}/> <Placeholder xs={8}/>
+                                <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+                                <Placeholder xs={6} /> <Placeholder xs={8} />
                             </Placeholder>
-                            <Placeholder.Button variant="primary" xs={6}/>
+                            <Placeholder.Button variant="primary" xs={6} />
                         </Card.Body>
                     </Card>
                 </Stack>);
             case "success":
-                return (<Stack direction="horizontal" gap={3}>
+                return (<Row>
                     {listRecipeCall.data.map((recipe) => {
 
                         return (
-                            <Card style={{width: '18rem'}}>
-                                <Card.Img variant="top" src=""/>
+                            <Col>
+                            <Card style={{ width: '18rem' }}>
+                                //TODO IMAGE
                                 <Card.Body>
                                     <Card.Title>
                                         {recipe.name}
@@ -91,8 +94,8 @@ function Home() {
                                         {recipe.description}
                                     </Card.Text>
                                     <Button variant="primary"
-                                            onClick={() => navigate("/recipeDetail?id=" + recipe.id)}
-                                    >Read more</Button>
+                                        onClick={() => navigate("/recipeDetail?id=" + recipe.id)}
+                                    >Otevřít</Button>
                                     {/*{users.map(user => {*/}
                                     {/*    return (*/}
                                     {/*        <NavDropdown.Item onClick={() => changeUser(user.id)}>*/}
@@ -101,15 +104,20 @@ function Home() {
                                     {/*    )*/}
                                     {/*})}*/}
                                     {/*{*/}
-                                    {  (getClassroomsToShow(listRecipeCall.data).includes(recipe.id)) ? <Button variant="secondary">Edit</Button> : ""
+                                    {
+                                        (getClassroomsToShow(listRecipeCall.data).includes(recipe.id)) ? <Button variant="secondary">Editovat</Button> : ""
+                                    }
+                                    {
+                                        (getClassroomsToShow(listRecipeCall.data).includes(recipe.id)) ? <Button variant="danger">Smazat</Button> : ""
                                     }
                                 </Card.Body>
-                            </Card>);
+                            </Card>
+                            </Col>);
                     })}
-                </Stack>)
+                </Row>)
             case "error":
                 return (<div>
-                    <Icon size={1} path={mdiAlertOctagonOutline}/> Error
+                    <Icon size={1} path={mdiAlertOctagonOutline} /> Error
                 </div>);
             default:
                 return null;
