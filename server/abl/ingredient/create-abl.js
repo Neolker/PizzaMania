bd348af53482b19aabd7ea646fa917d8313c1168
milos-hynek <134530320+milos-hynek@users.noreply.github.com>
2023-06-01 13:41:05 +0200

    Gramatika
    Gramatika

--- server/abl/ingredient/create-abl.js
+++ server/abl/ingredient/create-abl.js
@@ -21,7 +21,7 @@ async function CreateAbl(req, res) {
       let ingredientCreated = await dao.createIngredient(ingredient);
       res.json(ingredientCreated);
     } else {
-      res.status(400).send({ "error": "Validation of the input failed: name and unit are required, minimal lenght: 1 characters in the name and 1 characters in the unit." });
+      res.status(400).send({ "error": "Validation of the input failed: name and unit are required, minimal lenght: 1 character in the name and 1 character in the unit." });
     }
   } catch (e) {
     res.status(500).send({ "error": e.message });
