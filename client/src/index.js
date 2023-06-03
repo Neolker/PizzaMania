25db4f69577cbd606f7dede7823253e3983849cd
Neolker <neolker@gmail.com>
2023-06-03 23:43:07 +0200

    Revert "Refactoring 2"
    This reverts commit ac73ec026079d0bc4f37da3fc848e41f5d4344b7.

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import "./index.scss";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import RecipeDetail from "./routes/RecipeDetail";
 import Home from "./routes/Home";
 import {UserProvider} from "./UserProvider";
@@ -22,3 +23,8 @@ root.render(
         </UserProvider>
     </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
